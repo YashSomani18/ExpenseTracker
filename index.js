f786de59c389356e9d585fda52ff812ae685ec8c
YashSomani18 <yashsomani18@gmail.com>
2023-12-29 02:09:43 +0530

    errorResolved

--- index.js
+++ index.js
@@ -6,38 +6,36 @@ import connectToDb from "./config/connectToDb.js";
 import userRoute from './routes/userRoute.js';
 import transactionRoute from './routes/transactionRoute.js';
 import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-//Connect to Database
+// Connect to Database
 connectToDb();
 
 const app = express();
 
-
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/transactions', transactionRoute);
 
+// Define __dirname in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-//routes
-//user route in the main node file
-app.use('/api/v1/users',userRoute);
+// Static files
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-//TRansaction route in the main node file
-app.use('/api/v1/transactions',transactionRoute);
+const PORT = process.env.PORT || 8080;
 
-
-//static files
-app.use(express.static(path.join(__dirname ,'./client/build')));
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
-const PORT =  process.env.PORT || 8080;
-
-
-app.listen(PORT , ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
-})
+});
